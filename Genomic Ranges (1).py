9553266c4af95048276b731cedcd0f4e027f36e5
Abubakr El Sobky <52166349+aelsobky@users.noreply.github.com>
2022-03-30 11:46:11 -0700

    Update Genomic Ranges (1).py

--- Genomic Ranges (1).py
+++ Genomic Ranges (1).py
@@ -1,26 +1,15 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pyranges as pr
-
-
-# In[47]:
-
-
 import os
+
+# Fetching the path of chr_all.bed (the file that has blastn query results)
 path = os.path.abspath("chr_all.bed")
+# converting the file into a pyrange object called (chr_all)
 chr_all = pr.read_bed(path, False, nrows=1258)
-#print(chr_all)
+# Fetching the path of chr_published.bed (the file that has the published its loci)
 path = os.path.abspath("chr_published.bed")
 chr_published = pr.read_bed(path, False, nrows=125)
-#print(chr_published)
+# converting the file into a pyrange object called (chr_published)
 print(chr_all.intersect(chr_published))
-
-
-# In[ ]:
 
 
 
